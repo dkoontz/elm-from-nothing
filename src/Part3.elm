bcecdad59395eecf9fa2b0215451778e0122642c
Joshua Grosso <jgrosso1@asu.edu>
2016-11-12 15:15:01 -0700

    Create Part3.elm

---
+++ src/Part3.elm
@@ -0,0 +1,8 @@
+module Part3 exposing (..)
+
+-- Get ready for your mind to be blown... we're gonna create our own types of data!
+-- Instead of just working with Int, Float, String, and the rest, we can create and use our own custom values.
+-- Let's dive into an example!
+
+-- We're in a top-secret nuclear facility, and let's say that we want to configure our alarm system to require a password.
+-- Let's make a passcodeRequired variable to store our decision so that we can use it in the rest of our system.
